0ce3bdd5f25895a74f0a74a7f284635fdc2335e2
Miguel michelson <miguelmichelson@gmail.com>
2022-06-17 16:14:30 -0400

    favicon

--- lib/rauversion/accounts/user.ex
+++ lib/rauversion/accounts/user.ex
@@ -109,7 +109,7 @@ defmodule Rauversion.Accounts.User do
   defp validate_password(changeset, opts) do
     changeset
     |> validate_required([:password])
-    |> validate_length(:password, min: 12, max: 72)
+    |> validate_length(:password, min: 6, max: 72)
     # |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
