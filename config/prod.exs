b4b7dcf02ee28c1acf946d1d45b986edfe9571d1
Miguel michelson <miguelmichelson@gmail.com>
2022-06-17 18:14:13 -0400

    endpoint host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rauversion, RauversionWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "rauversion.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
