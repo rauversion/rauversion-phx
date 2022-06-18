a76a5966dcde249a1c48839f02677e54429391a5
Miguel michelson <miguelmichelson@gmail.com>
2022-06-18 13:05:25 -0400

    fix user menu

--- test/rauversion_web/controllers/page_controller_test.exs
+++ test/rauversion_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RauversionWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Rauversion"
   end
 end
