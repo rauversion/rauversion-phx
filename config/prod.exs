a7205f3557b01dc913a63381417d6e48990e3478
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-06-24 13:07:05 -0400

    force ssl with x_forwarded_proto (#9)

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ import Config
 config :rauversion, RauversionWeb.Endpoint,
   url: [host: "rauversion.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 
 # Do not print debug messages in production
