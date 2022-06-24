eb84217440322f94d38215439db74c98f75f0df8
Miguel Michelson Martinez <miguel@chaskiq.io>
2022-06-24 13:06:15 -0400

    force ssl with x_forwarded_proto

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,9 @@ import Config
 # before starting your production server.
 config :rauversion, RauversionWeb.Endpoint,
   url: [host: "rauversion.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
 
 # Do not print debug messages in production
 config :logger, level: :info
