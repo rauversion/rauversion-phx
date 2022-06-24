db469212e3068adea7940d28f2db8aacbb6e0dc7
Miguel michelson <miguelmichelson@gmail.com>
2022-06-24 02:27:30 -0400

    share track component

--- lib/rauversion_web/live/track_live/form_component.ex
+++ lib/rauversion_web/live/track_live/form_component.ex
@@ -33,6 +33,7 @@ defmodule RauversionWeb.TrackLive.FormComponent do
   end
 
   # the save on create, upload step!, it will only bring an audio file
+  @impl true
   def handle_event(
         "save",
         %{"track" => track_params} = params,
@@ -56,6 +57,7 @@ defmodule RauversionWeb.TrackLive.FormComponent do
   end
 
   # info step handle submit
+  @impl true
   def handle_event(
         "save",
         %{} = track_params,
@@ -68,15 +70,44 @@ defmodule RauversionWeb.TrackLive.FormComponent do
     save_track(socket, socket.assigns.action, track_params)
   end
 
+  @impl true
   def handle_event("save", %{"track" => track_params}, socket) do
     track_params = track_params |> Map.put("user_id", socket.assigns.current_user.id)
     save_track(socket, socket.assigns.action, track_params)
   end
 
   defp save_track(socket, :edit, %{"track" => track_params}) do
-    # {completed, []} = uploaded_entries(socket, :cover)
-    # socket.assigns.uploads.cover
+    case handle_update(socket, track_params) do
+      {:ok, response} ->
+        {:noreply,
+         response
+         |> put_flash(:info, "Track updated successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
 
+      {:error, response} ->
+        {:noreply, response}
+    end
+  end
+
+  defp save_track(socket, :edit_from_create, %{"track" => track_params}) do
+    case handle_update(socket, track_params) do
+      {:ok, response} ->
+        step = %Step{name: "share", prev: "info", next: nil}
+
+        {:noreply,
+         response
+         |> put_flash(:info, "Track updated successfully")
+         |> assign(track: socket.assigns.track |> Rauversion.Repo.preload(:user))
+         |> assign(step: step)}
+
+      # |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, response} ->
+        {:noreply, response}
+    end
+  end
+
+  defp handle_update(socket, track_params) do
     track_params =
       track_params
       |> Map.put("cover", files_for(socket, :cover))
@@ -86,13 +117,10 @@ defmodule RauversionWeb.TrackLive.FormComponent do
 
     case Tracks.update_track(socket.assigns.track, track_params) do
       {:ok, _track} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Track updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+        {:ok, socket}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+        {:error, assign(socket, :changeset, changeset)}
     end
   end
 
@@ -121,7 +149,7 @@ defmodule RauversionWeb.TrackLive.FormComponent do
         end
 
       %{name: "info"} ->
-        save_track(socket, :edit, params)
+        save_track(socket, :edit_from_create, params)
 
       _ ->
         IO.inspect("NO STEP!!!")
