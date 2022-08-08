33257a3549e23a7aee6e3b75ce359c9f93f3d684
Miguel michelson <miguelmichelson@gmail.com>
2022-08-08 02:27:01 -0400

    other commit tweaks

--- lib/rauversion_web/live/articles_live/components/articles_list_component copy.ex
+++ lib/rauversion_web/live/articles_live/components/articles_list_component copy.ex
@@ -19,7 +19,7 @@ defmodule RauversionWeb.ArticlesLive.ArticlesListComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="bg-white dark:bg-black pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div class="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 dark:divide-gray-700 lg:max-w-7xl">
         <div>
           <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-4xl">Recent publications</h2>
