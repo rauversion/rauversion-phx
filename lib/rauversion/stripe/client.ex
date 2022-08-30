1a335a7a1b61c520d3fe2fede93df72b4cd45693
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-08-30 02:47:46 -0400

    stripe client

---
+++ lib/rauversion/stripe/client.ex
@@ -0,0 +1,171 @@
+defmodule Rauversion.Stripe.Client do
+  use Tesla
+
+  # "https://api.stripe.com/v1"
+  # "https://hookb.in/VGzMnEaPMziDebxDwpQl"
+  plug Tesla.Middleware.BaseUrl, "https://api.stripe.com/v1"
+
+  plug Tesla.Middleware.FormUrlencoded,
+    encode: &Plug.Conn.Query.encode/1,
+    decode: &Plug.Conn.Query.decode/1
+
+  # plug Tesla.Middleware.Headers, [{"authorization", "token xyz"}]
+  plug Tesla.Middleware.JSON
+
+  @moduledoc """
+  client = Rauversion.Stripe.Client.new
+  c = Rauversion.OauthCredentials.get_oauth_credential!(1)
+  Rauversion.Stripe.Client.accounts(client, c.uid)
+
+  ref: https://stripe.com/docs/connect/destination-charges
+  """
+
+  def new(token \\ nil) do
+    token =
+      case token do
+        nil ->
+          Application.get_env(:ueberauth, Ueberauth.Strategy.Stripe.OAuth)
+          |> Keyword.get(:client_secret)
+
+        t ->
+          t
+      end
+
+    Tesla.client([
+      {Tesla.Middleware.BearerAuth, token: token}
+    ])
+  end
+
+  def accounts(client, account) do
+    handle_response(get(client, "/accounts/" <> account))
+  end
+
+  def balance(client, account) do
+    prepare_request(client, :get, "/balance", account)
+  end
+
+  @doc """
+  PRODUCTS
+  """
+
+  @doc """
+  Rauversion.Stripe.Client.create_price(client, c.uid, %{name: "123})
+  """
+  def create_product(client, account, params) do
+    prepare_request(client, :post, "/products", account, params)
+  end
+
+  @doc """
+  Rauversion.Stripe.Client.get_price(client, c.uid, "xxxx")
+  """
+  def get_product(client, account, product_id) do
+    prepare_request(client, :get, "/products/#{product_id}", account)
+  end
+
+  def list_products(client, account, params) do
+    prepare_request(client, :get, "/products/", account, params)
+  end
+
+  @doc """
+  Rauversion.Stripe.Client.update_product(client, c.uid, "xxxxx", %{"metadata"=> %{"order_id"=> "6735"}})
+  """
+  def update_product(client, account, product_id, params) do
+    prepare_request(client, :post, "/products/#{product_id}", account, params)
+  end
+
+  # charges
+
+  @doc """
+
+   curl https://api.stripe.com/v1/payment_intents \
+   -u xxxxx: \
+   -d amount=1000 \
+   -d currency="usd" \
+   -d "automatic_payment_methods[enabled]"=true \
+   -d "transfer_data[amount]"="877"
+   -d "transfer_data[destination]"="{{CONNECTED_STRIPE_ACCOUNT_ID}}"
+
+  """
+
+  def payment_intent(
+        client,
+        params = %{
+          "amount" => _payment,
+          "currency" => _currency,
+          "automatic_payment_methods" => %{"enabled" => _enabled},
+          "transfer_data" => %{
+            "amount" => _amount,
+            "destination" => _destination
+          }
+        }
+      ) do
+    post(client, "/payment_intents", params, [])
+  end
+
+  @doc """
+  curl https://api.stripe.com/v1/refunds \
+  -u xxxxxx: \
+  -d charge="{CHARGE_ID}" \
+  -d reverse_transfer=true \
+  """
+
+  def payment_intent(
+        client,
+        params = %{
+          "charge" => _charge_id,
+          "reverse_transfer" => _
+        }
+      ) do
+    post(client, "/refunds", params, [])
+  end
+
+  def balance_transactions(client, account, limit \\ 3) do
+    prepare_request(client, :get, "/balance_transactions", account, %{"limit" => limit})
+  end
+
+  def list_charges(client, account, limit \\ 3) do
+    prepare_request(client, :get, "/charges", account, %{"limit" => limit})
+  end
+
+  def delete_product(client, account, product_id) do
+    prepare_request(client, :delete, "/products/#{product_id}", account)
+  end
+
+  defp prepare_request(client, method, path, account, params \\ [])
+
+  defp prepare_request(client, :post, path, account, params) do
+    opts = [
+      headers: [
+        {"Stripe-Account", account}
+      ]
+    ]
+
+    handle_response(post(client, path, params, opts))
+  end
+
+  defp prepare_request(client, :get, path, account, params) do
+    opts = [
+      headers: [{"Stripe-Account", account}],
+      query: params
+    ]
+
+    handle_response(get(client, path, opts))
+  end
+
+  defp prepare_request(client, :delete, path, account, params) do
+    opts = [
+      headers: [{"Stripe-Account", account}],
+      query: params
+    ]
+
+    handle_response(delete(client, path, opts))
+  end
+
+  defp handle_response({:ok, %{body: %{"error" => error}}}) do
+    {:error, error}
+  end
+
+  defp handle_response({:ok, %{body: body}}) do
+    {:ok, body}
+  end
+end
