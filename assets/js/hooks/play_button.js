44cd58dd66d556fdc7c452ef936070c1f602116f
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-08-30 02:48:14 -0400

    destroy callback on track hook

--- assets/js/hooks/play_button.js
+++ assets/js/hooks/play_button.js
@@ -1,20 +1,17 @@
 PlayButton = {
   mounted(){
     this.playBtn = this.el.querySelector('[data-play-button="button"]')
-
     this.playBtnListener = (e)=>{
-      console.log("OELIII")
       this.play()
     }
     this.playBtn.addEventListener("click", this.playBtnListener)
   },
   play(){
-    console.log("ADD FROM PLAYER")
+    // console.log("ADD FROM PLAYER")
     this.pushEventTo("#main-player", "play-song", {id: this.el.dataset.audioId } )
   },
   destroyed(){
     this.playBtn && this.playBtn.addEventListener("click", this.playBtnListener)
-
   }
 }
 
