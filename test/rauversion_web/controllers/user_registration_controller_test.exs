c7908f070bc41aaac038a6771b9d9dbfce539145
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-09-03 21:59:47 -0400

    controller tests

--- test/rauversion_web/controllers/user_registration_controller_test.exs
+++ test/rauversion_web/controllers/user_registration_controller_test.exs
@@ -42,13 +42,13 @@ defmodule RauversionWeb.UserRegistrationControllerTest do
     test "render errors for invalid data", %{conn: conn} do
       conn =
         post(conn, Routes.user_registration_path(conn, :create), %{
-          "user" => %{"email" => "with spaces", "password" => "too short"}
+          "user" => %{"email" => "with spaces", "password" => "short"}
         })
 
       response = html_response(conn, 200)
       assert response =~ "Register"
       assert response =~ "must have the @ sign and no spaces"
-      assert response =~ "should be at least 12 character"
+      assert response =~ "should be at least 6 character"
     end
   end
 end
