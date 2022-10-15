58575b730cd0891fa8ac636ddf2da8ab7de24150
Miguel michelson <miguelmichelson@gmail.com>
2022-10-15 17:00:48 -0300

    https on phx

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rauversion, RauversionWeb.Endpoint,
-  url: [host: "rauversion.com", port: 443],
+  url: [host: "rauversion.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
