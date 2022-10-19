3b46c284c0bbfcdf53e98de79d5b0d6dc2ebf64d
Camilo Castro <camilo@ninjas.cl>
2022-10-19 20:15:39 -0300

    added check for ffmpeg

--- mix.exs
+++ mix.exs
@@ -116,7 +116,7 @@ defmodule Rauversion.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      setup: ["check", "deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
