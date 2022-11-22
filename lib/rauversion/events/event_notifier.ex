950496666aa23a3390ce3337ea3dcba9afbe35a4
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-11-22 15:41:27 -0300

    More event tweaks (#119)
    * unfollowed list + artist scope
    
    * event ticket component, accounts
    
    * redirect on event resource not available
    
    * list users
    
    * ticket form component + profile/playlist tweaks
    
    * event tickets jsonb queries, filter start end time & hidden
    
    * purchase orders, display errors
    
    * fix dropdown toggle
    
    * event ticket notification
    
    * gettext, tracks live
    
    * ticket invitation
    
    * some translations
    
    * streaming link
    
    * gettxt
    
    * timezone for schedulings
    
    * event tweaks, tickets visibility
    
    * attachments ready

--- lib/rauversion/events/event_notifier.ex
+++ lib/rauversion/events/event_notifier.ex
@@ -17,6 +17,7 @@ defmodule Rauversion.Events.EventNotifier do
       |> to(recipient)
       |> from({"Rauversion", System.get_env("EMAIL_ACCOUNT", "foo@bar.com")})
       |> subject(subject)
+      |> add_attachments(options[:attachments])
       |> render_body(template, options)
 
     with {:ok, _metadata} <- Mailer.deliver(email) do
@@ -50,16 +51,33 @@ defmodule Rauversion.Events.EventNotifier do
       |> Repo.all()
       |> Repo.preload(event_ticket: [:event])
 
-    deliver(user.email, gettext("Event Tickets confirmation"), [
-      "deliver_event_tickets.html",
-      %{
-        user: user,
-        tickets: tickets,
-        purchase_order: purchase_order,
-        message: message,
-        inviter: inviter
-      }
-    ])
+    attachments =
+      tickets
+      |> Enum.map(fn t ->
+        Swoosh.Attachment.new(
+          {:data, Rauversion.PurchasedTickets.qr_png(t)},
+          # cid: "qrcode-#{t.id}.png",
+          filename: "qrcode-#{t.id}.png",
+          content_type: "image/png",
+          type: :inline
+        )
+      end)
+
+    deliver(
+      user.email,
+      gettext("Event Tickets confirmation"),
+      [
+        "deliver_event_tickets.html",
+        %{
+          user: user,
+          tickets: tickets,
+          purchase_order: purchase_order,
+          message: message,
+          inviter: inviter,
+          attachments: attachments
+        }
+      ]
+    )
   end
 
   def deliver_event_streaming_url(user, event) do
@@ -73,5 +91,16 @@ defmodule Rauversion.Events.EventNotifier do
 
     ==============================
     """)
+  end
+
+  defp add_attachments(email, attachments) do
+    if Enum.any?(attachments) do
+      attachments
+      |> Enum.reduce(email, fn att, acc ->
+        acc |> attachment(att)
+      end)
+    else
+      email
+    end
   end
 end
