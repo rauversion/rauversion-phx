0adabf558ea3b8c4e3a71e29800b23d3c8a060ee
Miguel Michelson <miguelmichelson@gmail.com>
2022-12-12 03:07:45 -0300

    ensure started ssl on rel migrate

--- lib/rauversion/release.ex
+++ lib/rauversion/release.ex
@@ -6,6 +6,7 @@ defmodule Rauversion.Release do
   @app :rauversion
 
   def migrate do
+    ensure_started()
     load_app()
 
     for repo <- repos() do
@@ -16,6 +17,10 @@ defmodule Rauversion.Release do
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp ensure_started do
+    Application.ensure_all_started(:ssl)
   end
 
   defp repos do
